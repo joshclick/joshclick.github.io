01e26a0c39275d216791ff11b916b5601ab4ddd5
Josh Click <yq272@nyu.edu>
2013-07-11 01:19:59 -0400

    compass works automatically now, changed content, seeing if compass runs on Github pages properly

--- _sass/config.rb
+++ _sass/config.rb
@@ -1,10 +1,12 @@
 require 'zurb-foundation'
 # Require any additional compass plugins here.
 
-# path to sass files
-sass_dir = "/"
-# path to where css should be generated
+http_path = "/"
+sass_dir = "./"
 css_dir = "../css"
+images_dir = "../img"
+javascripts_dir = "../js"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
